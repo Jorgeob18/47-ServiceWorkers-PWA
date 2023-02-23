613b90b382c9db8616e5c438d2e676502c28a0bc
Jorgeob18 <jorgealbertoob18@gmail.com>
2023-02-23 10:54:28 -0600

    Hacer una PWA instalable y cachear archivos

--- sw.js
+++ sw.js
@@ -1,8 +1,27 @@
+const nombreCache = 'apv-v1';
+const assets = [
+  './',
+  './index.html',
+  './css/bootstrap.css',
+  './css/styles.css',
+  './js/app.js',
+  './js/apv.js',
+];
+
 // Cuando se instala el Service Worker
 self.addEventListener('install', (e) => {
-  console.log('Instalado Service Worker');
+  console.log('Instalado el Service Worker');
 
-  console.log(e);
+  e.waitUntil(
+    // Buen lugar para cachear -
+    caches.open(nombreCache).then((cache) => {
+      // Esta función es asincrona...
+      console.log('cacheando...');
+      cache.addAll(assets);
+    })
+  );
+
+  // Revisar en App (Chrome) en Firefox en Almacenamiento
 });
 
 // Activar el service Worker
@@ -10,4 +29,10 @@ self.addEventListener('activate', (e) => {
   console.log('Service Worker activated');
 
   console.log(e);
+});
+
+// Evento fetch para descargar archivos estaticos
+
+self.addEventListener('fetch', (e) => {
+  console.log('Fetching Service Worker', e);
 });
